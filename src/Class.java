4d2bdb94d33edc402afe88d6cd6b1c2f05dbd3ca
dasta <arzymatodastan@gmail.com>
2022-05-13 17:43:10 +0600

    homework13

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Class {
+import java.util.Arrays;
+
+public class Class {
+    private  int number;
+    private  String soz;
+    private int[] massiv;
+
+    public Class(int number, String soz, int[] array) {
+        this.number = number;
+        this.soz = soz;
+        this.massiv = array;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getSoz() {
+        return soz;
+    }
+
+    public void setSoz(String soz) {
+        this.soz = soz;
+    }
+
+    public int[] getMassiv() {
+        return massiv;
+    }
+
+    public void setMassiv(int[] array) {
+        this.massiv = array;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "number=" + number +
+                ", soz='" + soz + '\'' +
+                ", array=" + Arrays.toString(massiv) +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Class clas=new Class(20,"Dastan",new int[]{23,45,1234,7,9,88,4});
+        System.out.println(clas);
+
     }
 }
